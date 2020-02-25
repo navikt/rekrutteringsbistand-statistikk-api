24935efe2caefb2bddecd38253c5f017b53b079e
Joar Aurdal <joar.aurdal@nav.no>
2020-02-25 16:28:36 +0100

    la til kandidatstatus endepunkt

--- src/Application.kt
+++ src/Application.kt
@@ -2,10 +2,19 @@ package no.nav
 
 import io.ktor.application.*
 import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.JacksonConverter
+import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.routing.routing
+import no.nav.rekrutteringsbistand.statistikk.kandidatstatus
+import java.time.LocalDateTime
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -13,10 +22,14 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     install(CallLogging)
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, JacksonConverter())
+    }
 
     routing {
         route("/rekrutteringsbistand-statistikk-api") {
             naisEndepunkt()
+            kandidatstatus()
 
             get("/") {
                 call.respondText("Hello World!")
@@ -24,4 +37,6 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 }
+
+
 
---
+++ src/rekrutteringsbistand/statistikk/Kandidatstatus.kt
@@ -0,0 +1,27 @@
+package no.nav.rekrutteringsbistand.statistikk
+
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.routing.post
+import io.ktor.routing.route
+import java.time.LocalDateTime
+
+fun Route.kandidatstatus() {
+    route("/kandidatstatus") {
+        post("/" ) {
+            val kandidatstatus = call.receive<Kandidatstatus>()
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+}
+
+data class Kandidatstatus(
+        val aktorid: Number,
+        val utfall: String,
+        val navident: String,
+        val enhetsnr: Number,
+        val tidspunkt: String
+)
