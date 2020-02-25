e0e98eaf8119a8e26ebfe3e87d220a0b036973c6
Joar Aurdal <joar.aurdal@nav.no>
2020-02-25 13:15:53 +0100

    La inn et hello world endepunkt

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,26 @@
 package no.nav
 
 import io.ktor.application.*
+import io.ktor.http.ContentType
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+        }
+    }
+    server.start(wait = true)
 }
 
