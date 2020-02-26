56056170a24ba2d1934cf5058559d7983ebce524
André Mossige <mossige.a@gmail.com>
2020-02-26 10:56:09 +0100

    Legg til Dockerfile og nais yaml

--- src/Application.kt
+++ src/Application.kt
@@ -16,11 +16,11 @@ import io.ktor.server.netty.Netty
 import no.nav.rekrutteringsbistand.statistikk.kandidatutfall.kandidatutfall
 import no.nav.rekrutteringsbistand.statistikk.nais.naisEndepunkt
 
-fun main(args: Array<String>) {
+fun main() {
+    val profil: String = System.getenv("PROFIL") ?: "lokal"
     val server = embeddedServer(
         Netty,
-        // TODO: Ikke watching kjør i prod
-        watchPaths = listOf("/"),
+        watchPaths = if (profil == "lokal") listOf("/no/nav/rekrutteringsbistand/statistikk") else emptyList(),
         port = 8080,
         module = Application::module
     )
