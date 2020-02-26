407c9c7bfbb6b469adb9a5aaa80f0598853273d4
André Mossige <mossige.a@gmail.com>
2020-02-26 08:20:38 +0100

    Sett opp autoreload

--- src/Application.kt
+++ src/Application.kt
@@ -2,16 +2,26 @@ package no.nav
 
 import io.ktor.application.*
 import io.ktor.features.CallLogging
+import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.route
 import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    val server = embeddedServer(
+        Netty,
+        // TODO: Ikke watching kjør i prod
+        watchPaths = listOf("/"),
+        port = 8080,
+        module = Application::module
+    )
+    server.start(wait = true)
+}
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     install(CallLogging)
 
     routing {
@@ -19,9 +29,9 @@ fun Application.module(testing: Boolean = false) {
             naisEndepunkt()
 
             get("/") {
-                call.respondText("Hello World!")
+                call.respondText("{\"test\": 1337}", ContentType.Application.Json)
             }
+
         }
     }
 }
-
