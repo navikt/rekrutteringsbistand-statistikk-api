b263f1f71ae10a38cf603e30919c46ee43dd504f
André Mossige <mossige.a@gmail.com>
2020-02-27 17:26:15 +0100

    Test nytt oppsett av db config

--- src/db/Database.kt
+++ src/db/Database.kt
@@ -5,38 +5,40 @@ import com.zaxxer.hikari.HikariDataSource
 import no.nav.rekrutteringsbistand.statistikk.utils.Environment
 import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil
 import org.flywaydb.core.Flyway
+import java.lang.RuntimeException
 import java.sql.Connection
 
 class Database(env: Environment) : DatabaseInterface {
+
+    data class DbConf(val mountPath: String, val jdbcUrl: String)
+
+    private val config = when (env.profil) {
+        "dev" -> DbConf(
+            mountPath = "postgresql/preprod-fss",
+            jdbcUrl = "jdbc:postgresql://b27dbvl009.preprod.local:5432/rekrutteringsbistand-statistikk"
+        )
+        "prod" -> DbConf(
+            mountPath = "postgresql/prod-fss",
+            jdbcUrl = "jdbc:postgresql://A01DBVL011.adeo.no:5432/rekrutteringsbistand-statistikk"
+        )
+        else -> throw RuntimeException("Feil ved oppsett av database config, ukjent profil: ${env.profil}")
+    }
+
     private val dataSource: HikariDataSource
 
     override val connection: Connection
         get() = dataSource.connection
 
     init {
-        val mountPath: String =
-            when (env.profil) {
-                "dev" -> "postgresql/preprod-fss"
-                "prod" -> "postgresql/prod-fss"
-                else -> throw RuntimeException("Vault mountPath variabel må være satt")
-            }
-
-        val dbJdbcUrl: String =
-            when (env.profil) {
-                "dev" -> "jdbc:postgresql://b27dbvl009.preprod.local:5432/rekrutteringsbistand-statistikk"
-                "prod" -> "jdbc:postgresql://A01DBVL011.adeo.no:5432/rekrutteringsbistand-statistikk"
-                else -> throw RuntimeException("Database JDBC-URL må være satt")
-            }
-
         val hikariConfig = HikariConfig().apply {
-            jdbcUrl = dbJdbcUrl
+            jdbcUrl = config.jdbcUrl
             minimumIdle = 1
             maximumPoolSize = 2
         }
 
         dataSource = HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(
             hikariConfig,
-            mountPath,
+            config.mountPath,
             "rekrutteringsbistand-statistikk-user"
         )
 
