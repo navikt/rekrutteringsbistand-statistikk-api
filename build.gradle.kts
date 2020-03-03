32d08bd9e52ca3708a562d0e501ee115de05f661
André Mossige <andre.mossige@nav.no>
2020-03-03 10:06:54 +0100

    Bytt over til kotlin dsl (#8)

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+val kotlinCodeStyle = "official"
+val logbackVersion = "1.2.1"
+val ktorVersion = "1.3.0"
+val kotlinVersion = "1.3.61"
+val h2Version = "1.4.200"
+val flywayVersion = "6.2.4"
+val hikariVersion = "3.4.2"
+val logstashEncoderVersion = "6.3"
+val vaultJdbcVersion = "1.3.1"
+val shadowVersion = "5.2.0"
+val postgresVersion = "42.2.10"
+val tokenValidationKtorVersion = "1.1.4"
+val tokenValidationTestSupportVersion = "1.1.4"
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.61"
+
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+apply(plugin = "kotlin")
+apply(plugin = "application")
+apply(plugin = "com.github.johnrengelman.shadow")
+
+application {
+    mainClassName = "no.nav.rekrutteringsbistand.statistikk.ApplicationKt"
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("src")
+kotlin.sourceSets["test"].kotlin.srcDirs("test")
+sourceSets["main"].resources.srcDirs("resources")
+sourceSets["test"].resources.srcDirs("testresources")
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-jackson:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+
+    implementation("com.h2database:h2:$h2Version")
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("com.zaxxer:HikariCP:$hikariVersion")
+    implementation("no.nav:vault-jdbc:$vaultJdbcVersion")
+
+    implementation("no.nav.security:token-validation-ktor:$tokenValidationKtorVersion")
+    implementation("no.nav.security:token-validation-test-support:$tokenValidationTestSupportVersion") {
+        exclude(group = "org.springframework.boot")
+    }
+
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+}
--- src/Application.kt
+++ src/Application.kt
@@ -1,16 +1,12 @@
 package no.nav.rekrutteringsbistand.statistikk
 
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.auth.Authentication
-import io.ktor.auth.authenticate
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.jackson.JacksonConverter
-import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.route
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
