d39662f8449bcd7e2842803f6f67e5d94cf0a0f3
André Mossige <andre.mossige@nav.no>
2020-03-04 14:57:47 +0100

    Del opp app i egen versjon for lokal kjøring og en for kjøring i miljø (#10)
    * Del opp app i lokal og miljø versjon
    
    * Fjern 'LOKALT' fra cluster
    
    * Flytt filer for lokal kjøring til test pakke
    
    * Unngå støy når man starter appen lokalt
    
    * Rydd i konfig av tokenvalidering
    
    * Fiks pakkene i test sources

---
+++ src/application/ApplicationEngine.kt
@@ -0,0 +1,38 @@
+package no.nav.rekrutteringsbistand.statistikk.application
+
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.jackson.JacksonConverter
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import io.ktor.server.engine.ApplicationEngine
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.util.KtorExperimentalAPI
+import no.nav.rekrutteringsbistand.statistikk.db.DatabaseInterface
+import no.nav.rekrutteringsbistand.statistikk.kandidatutfall.kandidatutfall
+import no.nav.rekrutteringsbistand.statistikk.nais.naisEndepunkt
+
+@KtorExperimentalAPI
+fun lagApplicationEngine(
+    database: DatabaseInterface,
+    tokenValidationConfig: Authentication.Configuration.() -> Unit
+): ApplicationEngine {
+    return embeddedServer(Netty, port = 8080) {
+        install(CallLogging)
+        install(ContentNegotiation) {
+            register(ContentType.Application.Json, JacksonConverter())
+        }
+        install(Authentication, tokenValidationConfig)
+
+        routing {
+            route("/rekrutteringsbistand-statistikk-api") {
+                kandidatutfall()
+                naisEndepunkt()
+            }
+        }
+    }
+}
--- src/auth/TokenSupportUtil.kt
+++ src/auth/TokenSupportUtil.kt
@@ -1,30 +1,23 @@
 package no.nav.rekrutteringsbistand.statistikk.auth
 
 import io.ktor.util.KtorExperimentalAPI
-import no.nav.rekrutteringsbistand.statistikk.utils.Miljø
-import no.nav.rekrutteringsbistand.statistikk.utils.Environment
+import no.nav.rekrutteringsbistand.statistikk.utils.Cluster
 import no.nav.security.token.support.ktor.IssuerConfig
 import no.nav.security.token.support.ktor.TokenSupportConfig
 
-@KtorExperimentalAPI
-class AuthenticationConfig {
+class TokenSupportUtil {
 
     companion object {
-        fun tokenSupportConfig(env: Environment): TokenSupportConfig {
-            val issuerConfig = when (env.miljø) {
-                Miljø.LOKALT -> IssuerConfig(
-                    name = "isso",
-                    discoveryUrl = "http://metadata",
-                    acceptedAudience = listOf("aud-localhost", "aud-isso"),
-                    cookieName = "isso-idtoken"
-                )
-                Miljø.DEV_FSS -> IssuerConfig(
+        @KtorExperimentalAPI
+        fun tokenSupportConfig(cluster: Cluster): TokenSupportConfig {
+            val issuerConfig = when (cluster) {
+                Cluster.DEV_FSS -> IssuerConfig(
                     name = "isso",
                     discoveryUrl = "https://login.microsoftonline.com/NAVQ.onmicrosoft.com/.well-known/openid-configuration",
                     acceptedAudience = listOf("38e07d31-659d-4595-939a-f18dce3446c5"),
                     cookieName = "isso-idtoken"
                 )
-                Miljø.PROD_FSS -> IssuerConfig(
+                Cluster.PROD_FSS -> IssuerConfig(
                     name = "isso",
                     discoveryUrl = "https://login.microsoftonline.com/navno.onmicrosoft.com/.well-known/openid-configuration",
                     acceptedAudience = listOf("9b4e07a3-4f4c-4bab-b866-87f62dff480d"),
