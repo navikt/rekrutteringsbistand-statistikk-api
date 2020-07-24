d2cb094fe4861ca94b856be370187d74fb23ed3b
André Mossige <mossige.a@gmail.com>
2020-07-24 10:37:17 +0200

    Stub sending til Kafka i prod

--- src/Application.kt
+++ src/Application.kt
@@ -3,7 +3,7 @@ package no.nav.rekrutteringsbistand.statistikk
 import io.ktor.auth.Authentication
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.rekrutteringsbistand.statistikk.db.Database
-import no.nav.rekrutteringsbistand.statistikk.kafka.DatavarehusKafkaProducerImpl
+import no.nav.rekrutteringsbistand.statistikk.kafka.DatavarehusKafkaProducerStub
 import no.nav.security.token.support.ktor.tokenValidationSupport
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -22,7 +22,7 @@ fun main() {
     val applicationEngine = lagApplicationEngine(
         database = database,
         tokenValidationConfig = tokenValidationConfig,
-        datavarehusKafkaProducer = DatavarehusKafkaProducerImpl("todo") // TODO
+        datavarehusKafkaProducer = DatavarehusKafkaProducerStub()
     )
     applicationEngine.start()
     log.info("Applikasjon startet i miljø: ${Cluster.current}")
