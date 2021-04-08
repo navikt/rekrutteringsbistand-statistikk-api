607c555915286c918984587d87c539e91851c041
Joar Aurdal <joar.aurdal@nav.no>
2021-04-08 16:18:55 +0200

    Hent ut intervalldatoer for hull

--- src/datakatalog/SendHullICvTilDatakatalog.kt
+++ src/datakatalog/SendHullICvTilDatakatalog.kt
@@ -3,9 +3,16 @@ package no.nav.rekrutteringsbistand.statistikk.datakatalog
 import no.nav.rekrutteringsbistand.statistikk.db.Repository
 import no.nav.rekrutteringsbistand.statistikk.log
 import java.time.LocalDate
+import java.util.*
+
 
 fun sendHullICvTilDatakatalog(repository: Repository) = Runnable {
     log.info("Henter data for hull for katakatalog")
+    val fraDato = LocalDate.of(2021, 4, 6)
+    val datoer: List<LocalDate> = finnDagerFra(fraDato)
+
+    log.info("Henter data for hull for katakatalog for dager: $datoer")
+
     val iDag = LocalDate.now()
     val fåttJobbenMedHull = repository.hentAntallFåttJobben(harHull = true, iDag.minusYears(5), iDag.plusYears(1))
     val fåttJobbenUtenHull = repository.hentAntallFåttJobben(harHull = false, iDag.minusYears(5), iDag.plusYears(1))
@@ -16,13 +23,19 @@ fun sendHullICvTilDatakatalog(repository: Repository) = Runnable {
     val presentertUtenHull = repository.hentAntallPresentert(harHull = false, iDag.minusYears(5), iDag.plusYears(1))
     val presentertUkjentHull = repository.hentAntallPresentert(harHull = null, iDag.minusYears(5), iDag.plusYears(1))
 
-    log.info("Har hentet data for hull for datakatalog: " +
-            "fåttJobbenMedHull: $fåttJobbenMedHull, " +
-            "fåttJobbenUtenHull: $fåttJobbenUtenHull, " +
-            "fåttJobbenUkjentHull: $fåttJobbenUkjentHull, " +
-            "presentertMedHull: $presentertMedHull, " +
-            "presentertUtenHull: $presentertUtenHull, " +
-            "presentertUkjentHull: $presentertUkjentHull")
+    log.info(
+        "Har hentet data for hull for datakatalog: " +
+                "fåttJobbenMedHull: $fåttJobbenMedHull, " +
+                "fåttJobbenUtenHull: $fåttJobbenUtenHull, " +
+                "fåttJobbenUkjentHull: $fåttJobbenUkjentHull, " +
+                "presentertMedHull: $presentertMedHull, " +
+                "presentertUtenHull: $presentertUtenHull, " +
+                "presentertUkjentHull: $presentertUkjentHull"
+    )
 
     return@Runnable
 }
+
+private tailrec fun finnDagerFra(dato: LocalDate): List<LocalDate> =
+    if (dato.isAfter(LocalDate.now())) emptyList()
+    else listOf(dato) + finnDagerFra(dato.plusDays(1))
