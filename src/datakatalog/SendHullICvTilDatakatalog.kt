b54ba4e5a43b7a12fc5abf178ce2f92710b7bdea
Joar Aurdal <joar.aurdal@nav.no>
2021-04-09 12:31:45 +0200

    Legg til en plotty for hull

--- src/datakatalog/SendHullICvTilDatakatalog.kt
+++ src/datakatalog/SendHullICvTilDatakatalog.kt
@@ -1,7 +1,6 @@
 package no.nav.rekrutteringsbistand.statistikk.datakatalog
 
-import kscience.plotly.Plotly
-import kscience.plotly.histogram
+import kscience.plotly.*
 import no.nav.rekrutteringsbistand.statistikk.db.Repository
 import no.nav.rekrutteringsbistand.statistikk.log
 import java.time.LocalDate
@@ -15,7 +14,7 @@ fun sendHullICvTilDatakatalog(repository: Repository) = Runnable {
 
     log.info("Henter data for hull for katakatalog for dager: $datoer")
 
-    datoer.map {
+    datoer.forEach {
         val fåttJobbenMedHull = repository.hentAntallFåttJobben(harHull = true, it, it.plusDays(1))
         val fåttJobbenUtenHull = repository.hentAntallFåttJobben(harHull = false, it, it.plusDays(1))
         val fåttJobbenUkjentHull = repository.hentAntallFåttJobben(harHull = null, it, it.plusDays(1))
@@ -23,15 +22,6 @@ fun sendHullICvTilDatakatalog(repository: Repository) = Runnable {
         val presentertMedHull = repository.hentAntallPresentert(harHull = true, it, it.plusDays(1))
         val presentertUtenHull = repository.hentAntallPresentert(harHull = false, it, it.plusDays(1))
         val presentertUkjentHull = repository.hentAntallPresentert(harHull = null, it, it.plusDays(1))
-
-
-
-        Plotly.plot {
-            this.histogram {
-
-            }
-        }
-
 
         log.info(
             "Har hentet data for hull for datakatalog for $it: " +
@@ -44,9 +34,51 @@ fun sendHullICvTilDatakatalog(repository: Repository) = Runnable {
         )
     }
 
+    val plot = Plotly.plot {
+        histogram {
+            x.strings = datoer.map { it.toString() }
+            y.numbers = datoer.map { repository.hentAntallPresentert(harHull = true, it, it.plusDays(1)) }
+            name = "Antall presentert med hull"
+        }
+
+        getLayout()
+    }
+
+    log.info("Plotty: ${plot.toJsonString()}")
 
 
     return@Runnable
+}
+
+
+
+private fun Plot.getLayout() {
+    layout {
+        bargap = 0.1
+        title {
+            text = "Basic Histogram"
+            font {
+                size = 20
+                color("black")
+            }
+        }
+        xaxis {
+            title {
+                text = "Dato"
+                font {
+                    size = 16
+                }
+            }
+        }
+        yaxis {
+            title {
+                text = "Antall"
+                font {
+                    size = 16
+                }
+            }
+        }
+    }
 }
 
 private tailrec fun finnDagerFra(dato: LocalDate): List<LocalDate> =
