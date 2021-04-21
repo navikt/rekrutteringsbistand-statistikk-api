1f955b5698599a919bea3eb810a69dfbda4d4a43
Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
2021-04-21 15:04:44 +0200

    Legg til andel av hull i CV. (Manglet fil)

---
+++ src/datakatalog/HullDatagrunnlag.kt
@@ -0,0 +1,20 @@
+package no.nav.rekrutteringsbistand.statistikk.datakatalog
+
+import java.time.LocalDate
+
+class HullDatagrunnlag(
+    private val antallPresentertPerDagHarHull: Map<Pair<LocalDate, Boolean?>, Int>,
+    private val antallFåttJobbPerDagHarHull: Map<Pair<LocalDate, Boolean?>, Int>
+) {
+    fun hentAntallPresentert(harHull: Boolean?, dato: LocalDate) = antallPresentertPerDagHarHull[dato to harHull]
+        ?: throw RuntimeException("datagrunnlag eksisterer ikke for presenterte $dato med harHull $harHull")
+    fun hentAntallFåttJobben(harHull: Boolean?, dato: LocalDate) = antallFåttJobbPerDagHarHull[dato to harHull]
+        ?: throw RuntimeException("datagrunnlag eksisterer ikke for fått jobben $dato med harHull $harHull")
+
+    fun hentAndelPresentert(dato: LocalDate) = hentAntallPresentert(true, dato).let { antallMedHull ->
+        antallMedHull.toDouble() / (antallMedHull + hentAntallPresentert(false, dato))
+    }.let { if (it.isNaN()) 0.0 else it }
+    fun hentAndelFåttJobben(dato: LocalDate) = hentAntallFåttJobben(true, dato).let { antallMedHull ->
+        antallMedHull.toDouble() / (antallMedHull + hentAntallFåttJobben(false, dato))
+    }.let { if (it.isNaN()) 0.0 else it }
+}
