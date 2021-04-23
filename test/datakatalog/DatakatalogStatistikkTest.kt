1637374d495dbd4f39b25357298592dccee5cc8b
Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
2021-04-23 16:28:05 +0200

    Legg til datapakke-test, med 3 alternativer.

--- test/datakatalog/DatakatalogStatistikkTest.kt
+++ test/datakatalog/DatakatalogStatistikkTest.kt
@@ -11,7 +11,6 @@ import io.ktor.client.features.json.*
 import io.ktor.client.request.forms.*
 import io.ktor.http.*
 import io.ktor.utils.io.core.*
-import io.ktor.utils.io.core.internal.*
 import no.nav.rekrutteringsbistand.statistikk.Cluster
 import no.nav.rekrutteringsbistand.statistikk.datakatalog.DatakatalogKlient
 import no.nav.rekrutteringsbistand.statistikk.datakatalog.DatakatalogStatistikk
@@ -20,9 +19,9 @@ import no.nav.rekrutteringsbistand.statistikk.db.Repository
 import org.junit.Test
 import java.time.LocalDate
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import kotlin.text.String
 
-@DangerousInternalIoApi
 class DatakatalogStatistikkTest {
     companion object {
         private val database = TestDatabase()
@@ -92,23 +91,103 @@ class DatakatalogStatistikkTest {
             it.get(this)
         } as ArrayList<*>
 
-        return partListe.map { førstePart ->
-            val partLeseFunksjon = førstePart.javaClass.getDeclaredField("provider").let {
+        return partListe.map { part ->
+            val partLeseFunksjon = part.javaClass.getDeclaredField("provider").let {
                 it.trySetAccessible()
-                it.get(førstePart)
-            }
-            (partLeseFunksjon as () -> ByteReadPacket).invoke().readText()
+                it.get(part)
+            } as () -> ByteReadPacket
+            partLeseFunksjon().readText()
         }
     }
 
 
-    val DatakatalogStatistikk = DatakatalogStatistikk(
+    val datakatalogStatistikkVar = DatakatalogStatistikk(
         repository,
         DatakatalogKlient(client, DatakatalogUrl(Cluster.LOKAL)),
         dagensDato = { LocalDate.of(2021, 4, 20) })
 
     @Test
     fun testSendFil() {
-        DatakatalogStatistikk.run()
+        datakatalogStatistikkVar.run()
+    }
+
+    @Test
+    fun testDatapakkeMedBoolean() {
+        var kalt = false
+        val client = lagVerifiableHttpClient(datapakkeAsserts = { body ->
+            val expected =
+                """{"title":"Rekrutteringsbistand statistikk","description":"Vise rekrutteringsbistand statistikk","views":[{"title":"Antall hull i cv","description":"Vise antall hull i cv","specType":"plotly","spec":{"url":"antallhull.json"}},{"title":"Andel hull i cv","description":"Vise andel hull i cv","specType":"plotly","spec":{"url":"andelhull.json"}},{"title":"Antall alder presentert","description":"Vise antal alder presentert","specType":"plotly","spec":{"url":"alderPresentert.json"}},{"title":"Andel alder presentert","description":"Vise andel alder presentert","specType":"plotly","spec":{"url":"alderAndelPresentert.json"}},{"title":"Antall alder fått jobben","description":"Vise antal alder fått jobben","specType":"plotly","spec":{"url":"aalderFåttJobben.json"}},{"title":"Andel alder fått jobben","description":"Vise andel alder fått jobben","specType":"plotly","spec":{"url":"alderAndelFåttJobben.json"}}],"resources":[]}"""
+            assertEquals(expected, body)
+            kalt = true
+        })
+        val datakatalogStatistikk = lagDatakatalogStatistikk(client)
+        datakatalogStatistikk.run()
+        assertTrue(kalt)
+    }
+
+    @Test
+    fun testDatapakkeMedCallVerifier() {
+        val callVerifier = VerifiserbarFunksjon<String> { body ->
+            val expected =
+                """{"title":"Rekrutteringsbistand statistikk","description":"Vise rekrutteringsbistand statistikk","views":[{"title":"Antall hull i cv","description":"Vise antall hull i cv","specType":"plotly","spec":{"url":"antallhull.json"}},{"title":"Andel hull i cv","description":"Vise andel hull i cv","specType":"plotly","spec":{"url":"andelhull.json"}},{"title":"Antall alder presentert","description":"Vise antal alder presentert","specType":"plotly","spec":{"url":"alderPresentert.json"}},{"title":"Andel alder presentert","description":"Vise andel alder presentert","specType":"plotly","spec":{"url":"alderAndelPresentert.json"}},{"title":"Antall alder fått jobben","description":"Vise antal alder fått jobben","specType":"plotly","spec":{"url":"aalderFåttJobben.json"}},{"title":"Andel alder fått jobben","description":"Vise andel alder fått jobben","specType":"plotly","spec":{"url":"alderAndelFåttJobben.json"}}],"resources":[]}"""
+            assertEquals(expected, body)
+        }
+        val client = lagVerifiableHttpClient(datapakkeAsserts = callVerifier())
+        val datakatalogStatistikk = lagDatakatalogStatistikk(client)
+        datakatalogStatistikk.run()
+        callVerifier.verifiser()
+    }
+
+    @Test
+    fun testDatapakkeMedCallVerifierOneLiner() {
+        VerifiserbarFunksjon<String> { body ->
+            val expected =
+                """{"title":"Rekrutteringsbistand statistikk","description":"Vise rekrutteringsbistand statistikk","views":[{"title":"Antall hull i cv","description":"Vise antall hull i cv","specType":"plotly","spec":{"url":"antallhull.json"}},{"title":"Andel hull i cv","description":"Vise andel hull i cv","specType":"plotly","spec":{"url":"andelhull.json"}},{"title":"Antall alder presentert","description":"Vise antal alder presentert","specType":"plotly","spec":{"url":"alderPresentert.json"}},{"title":"Andel alder presentert","description":"Vise andel alder presentert","specType":"plotly","spec":{"url":"alderAndelPresentert.json"}},{"title":"Antall alder fått jobben","description":"Vise antal alder fått jobben","specType":"plotly","spec":{"url":"aalderFåttJobben.json"}},{"title":"Andel alder fått jobben","description":"Vise andel alder fått jobben","specType":"plotly","spec":{"url":"alderAndelFåttJobben.json"}}],"resources":[]}"""
+            assertEquals(expected, body)
+        }.apply {
+            lagVerifiableHttpClient(datapakkeAsserts = this())
+                .let(this@DatakatalogStatistikkTest::lagDatakatalogStatistikk)
+                .let(DatakatalogStatistikk::run)
+        }.verifiser()
+    }
+
+    class VerifiserbarFunksjon<T>(private val function: (T)->Unit) {
+        private var invoked = false
+
+        operator fun invoke(): (T) -> Unit = { parameter:T -> function(parameter).apply { invoked=true } }
+        fun verifiser() = assertTrue(invoked, "no.frodank.CallVerifier was never invoked before verified.")
+    }
+
+    private fun lagDatakatalogStatistikk(client: HttpClient) = DatakatalogStatistikk(
+        repository,
+        DatakatalogKlient(client, DatakatalogUrl(Cluster.LOKAL)),
+        dagensDato = { LocalDate.of(2021, 4, 20) })
+
+    private fun lagVerifiableHttpClient(attachementAsserts: (List<String>) -> Unit = {}, datapakkeAsserts: (String) -> Unit = {}) =
+        HttpClient(MockEngine) {
+        install(JsonFeature) {
+            serializer = JacksonSerializer {
+                registerModule(JavaTimeModule())
+                disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            }
+        }
+        engine {
+            addHandler { request ->
+                when (request.url.fullPath) {
+                    "/v1/datapackage/10d33ba3796b95b53ac1466015aa0ac7/attachments" -> {
+                        val multiPartFormDataContent = request.body as MultiPartFormDataContent
+                        val actualPartsInput = multiPartFormDataContent.hentPartsMedReflection()
+                        attachementAsserts(actualPartsInput)
+                        respond("")
+                    }
+                    "/v1/datapackage/10d33ba3796b95b53ac1466015aa0ac7" -> {
+                        datapakkeAsserts(String(request.body.toByteArray()))
+                        respond("")
+                    }
+
+                    else -> error("Unhandled ${request.url.fullPath}")
+                }
+            }
+        }
     }
 }
