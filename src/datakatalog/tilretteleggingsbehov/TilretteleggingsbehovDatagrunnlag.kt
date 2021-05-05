414fb90c516afdce78d68887e9a4ad3e45438dea
Mads Lee Giil <mads.lee.giil@nav.no>
2021-05-05 07:24:52 +0200

    Fjern utkommentert kode

--- src/datakatalog/tilretteleggingsbehov/TilretteleggingsbehovDatagrunnlag.kt
+++ src/datakatalog/tilretteleggingsbehov/TilretteleggingsbehovDatagrunnlag.kt
@@ -27,24 +27,3 @@ class TilretteleggingsbehovDatagrunnlag(
 private fun finnSpesifikt(tilretteleggingsbehov: String): (List<String>) -> Boolean = { tilretteleggingsbehov in it }
 private fun minstEtTilretteleggingsbehov(): (List<String>) -> Boolean = { it.isNotEmpty() }
 private fun totalAntall(): (List<String>) -> Boolean = { true }
-
-/*
-
-class HullDatagrunnlag(
-    private val antallPresentertPerDagHarHull: Map<Pair<LocalDate, Boolean?>, Int>,
-    private val antallFåttJobbPerDagHarHull: Map<Pair<LocalDate, Boolean?>, Int>
-) {
-    fun hentAntallPresentert(harHull: Boolean?, dato: LocalDate) = antallPresentertPerDagHarHull[dato to harHull]
-        ?: throw RuntimeException("datagrunnlag eksisterer ikke for presenterte $dato med harHull $harHull")
-    fun hentAntallFåttJobben(harHull: Boolean?, dato: LocalDate) = antallFåttJobbPerDagHarHull[dato to harHull]
-        ?: throw RuntimeException("datagrunnlag eksisterer ikke for fått jobben $dato med harHull $harHull")
-
-    fun hentAndelPresentert(dato: LocalDate) = hentAntallPresentert(true, dato).let { antallMedHull ->
-        antallMedHull.toDouble() / (antallMedHull + hentAntallPresentert(false, dato))
-    }.let { if (it.isNaN()) 0.0 else it }
-    fun hentAndelFåttJobben(dato: LocalDate) = hentAntallFåttJobben(true, dato).let { antallMedHull ->
-        antallMedHull.toDouble() / (antallMedHull + hentAntallFåttJobben(false, dato))
-    }.let { if (it.isNaN()) 0.0 else it }
-}
-
- */
