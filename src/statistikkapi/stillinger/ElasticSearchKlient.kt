719423357d38b386f929286872c5a69f6f02e544
frodank <11638682+frodank@users.noreply.github.com>
2021-05-12 12:20:39 +0200

    Refaktorere ElasticSearchKlient

--- src/statistikkapi/stillinger/ElasticSearchKlient.kt
+++ src/statistikkapi/stillinger/ElasticSearchKlient.kt
@@ -14,13 +14,16 @@ class ElasticSearchKlient(private val httpKlient: HttpClient = HttpClient(Apache
 
     // TODO: Metode for å hente alle stillinger med tilrettelegging/inkludering i gitt tidsrom
 
-    val stillingssokProxyDokumentUrl = when (Cluster.current) {
+    private val stillingssokProxyDokumentUrl = when (Cluster.current) {
         Cluster.PROD_FSS -> "https://rekrutteringsbistand-stillingssok-proxy.intern.nav.no/stilling/_doc"
         Cluster.DEV_FSS -> "https://rekrutteringsbistand-stillingssok-proxy.dev.intern.nav.no/stilling/_doc"
         Cluster.LOKAL -> "https://rekrutteringsbistand-stillingssok-proxy.dev.intern.nav.no/stilling/_doc"
     }
 
-    fun hentStilling(stillingUuid: String): ElasticSearchStilling? = getRequest("$stillingssokProxyDokumentUrl/$stillingUuid")
+    fun hentStilling(stillingUuid: String): ElasticSearchStilling? = runBlocking {
+        val esSvar: String = httpKlient.get("$stillingssokProxyDokumentUrl/$stillingUuid")
+        mapElasticSearchJsonSvarTilStilling(esSvar)
+    }
 
     private fun mapElasticSearchJsonSvarTilStilling(fulltElasticSearchSvarJson: String): ElasticSearchStilling? {
 
@@ -28,33 +31,26 @@ class ElasticSearchKlient(private val httpKlient: HttpClient = HttpClient(Apache
             .at("/hits/hits")[0]
             ?.at("/_source/stilling") ?: return null
 
-        val tags = hentTags(jsonStilling.at("/properties/tags").toList().map { it.asText() })
-        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+        val (inkluderingsmuligheter, prioriterteMålgrupper, tiltakEllerVirkemidler) = hentTags(jsonStilling.at("/properties/tags").toList().map { it.asText() })
 
-        // TODO: Sjekk datohåndtering
         return ElasticSearchStilling(
-            jsonStilling.at("/uuid").asText(),
-            LocalDateTime.parse(jsonStilling.at("/created").asText().substring(0, 19), dateTimeFormatter),
-            LocalDateTime.parse(jsonStilling.at("/published").asText().substring(0, 19), dateTimeFormatter),
-            tags.first,
-            tags.second,
-            tags.third
-        )
+                uuid = jsonStilling.at("/uuid").asText(),
+                opprettet = LocalDateTime.parse(jsonStilling.at("/created").asText()),
+                publisert = LocalDateTime.parse(jsonStilling.at("/published").asText()),
+                inkluderingsmuligheter = inkluderingsmuligheter,
+                prioriterteMålgrupper = prioriterteMålgrupper,
+                tiltakEllerEllerVirkemidler = tiltakEllerVirkemidler
+            )
     }
 
     private fun hentTags(ukategoriserteTags: List<String>): Triple<List<InkluderingTag>, List<PrioriterteMålgrupperTag>, List<TiltakEllerVirkemiddelTag>> =
         Triple(
-            ukategoriserteTags.filter { it.startsWith("INKLUDERING__") }.map { InkluderingTag.valueOf(it.removePrefix("INKLUDERING__")) },
-            ukategoriserteTags.filter { it.startsWith("PRIORITERT_MÅLGRUPPE__")}.map { PrioriterteMålgrupperTag.valueOf(it.removePrefix("PRIORITERT_MÅLGRUPPE__"))},
-            ukategoriserteTags.filter { it.startsWith("TILTAK_ELLER_VIRKEMIDDEL__")}.map { TiltakEllerVirkemiddelTag.valueOf(it.removePrefix("TILTAK_ELLER_VIRKEMIDDEL__"))}
+            ukategoriserteTags.filter { it.startsWith("INKLUDERING__") }
+                .map { InkluderingTag.valueOf(it.removePrefix("INKLUDERING__")) },
+            ukategoriserteTags.filter { it.startsWith("PRIORITERT_MÅLGRUPPE__") }
+                .map { PrioriterteMålgrupperTag.valueOf(it.removePrefix("PRIORITERT_MÅLGRUPPE__")) },
+            ukategoriserteTags.filter { it.startsWith("TILTAK_ELLER_VIRKEMIDDEL__") }
+                .map { TiltakEllerVirkemiddelTag.valueOf(it.removePrefix("TILTAK_ELLER_VIRKEMIDDEL__")) }
         )
 
-    private fun getRequest(url: String): ElasticSearchStilling? {
-        return runBlocking {
-            val client = httpKlient
-            val esSvar: String = client.get(url)
-            client.close()
-            mapElasticSearchJsonSvarTilStilling(esSvar)
-        }
-    }
 }
