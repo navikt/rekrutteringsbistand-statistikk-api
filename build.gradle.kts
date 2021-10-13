764fbb396461d385fefc35d23ab75b8ed421eccb
Are Husby <are.husby@nav.no>
2021-10-13 23:38:45 +0200

    Oppgrader noen flere avhengigheter (#53)
    * Oppgrader noen flere avhengigheter
    
    * Oppgrader kotlin-logging versjon
    
    * Oppgrader ktor versjon
    
    * Oppgrader plugin versjoner
    
    * Refaktorering
    
    * Refaktorering

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,39 @@
-val kotlinVersion =
-// Det er minst sannsynlighet for dependency-plunder når vi bruker samme versjon av Kotlin som den som er bundlet med Gradle via gradlew.
-// For å se hvilken versjon det er, kjør "./gradlew -version".
-// Kotlin-versjonen oppgraderes slik: https://docs.gradle.org/current/userguide/gradle_wrapper.html#sec:upgrading_wrapper
-    embeddedKotlinVersion
-
+/** Det er minst sannsynlighet for dependency-plunder når vi bruker samme versjon av Kotlin som den som er bundlet med Gradle via gradlew.
+For å se hvilken versjon det er, kjør "./gradlew --version".
+Kotlin-versjonen oppgraderes slik: https://docs.gradle.org/current/userguide/gradle_wrapper.html#sec:upgrading_wrapper
+ */
 val kotlinCodeStyle = "official"
-val logbackVersion = "1.2.3"
-val ktorVersion = "1.5.3"
+val logbackVersion = "1.2.6"
+val ktorVersion = "1.6.4"
 val h2Version = "1.4.200"
-val flywayVersion = "7.5.3"
-val hikariVersion = "4.0.2"
+val flywayVersion = "8.0.0"
+val hikariVersion = "5.0.0"
 val logstashEncoderVersion = "6.6"
 val vaultJdbcVersion = "1.3.7"
-val postgresVersion = "42.2.18"
-val tokenValidationVersion = "1.3.3"
+val postgresVersion = "42.2.24"
+val tokenValidationVersion = "1.3.8"
 val jacksonVersion = "2.13.0"
-val assertkVersion = "0.23.1"
-val micrometerPrometheusVersion = "1.6.3"
+val assertkVersion = "0.25"
+val micrometerPrometheusVersion = "1.7.4"
 val kafkaClientsVersion = "2.8.0"
 val mockkVersion = "1.12.0"
 val kafkaEmbeddedEnvironmentVersion = "2.8.0"
 val kafkaAvroSerializerVersion = "6.2.1"
-val shedlockVersion = "4.20.0"
-val pitestVersion = "1.5.2"
+val shedlockVersion = "4.28.0"
+val pitestVersion = "1.7.0"
 val elasticSearchClientVersion = "7.10.1"
-
+val kotlinLoggingVersion = "2.0.11"
+val jsonassertVersion = "1.5.0"
+val plotlyktServerVersion = "0.3.0"
 
 
 plugins {
     application
     kotlin("jvm") version embeddedKotlinVersion
-
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("com.github.ben-manes.versions") version "0.28.0"
-    id("com.github.davidmc24.gradle.plugin.avro") version "1.2.0"
-    id("info.solidsoft.pitest") version "1.5.2"
-
+    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.2.1"
+    id("info.solidsoft.pitest") version "1.7.0"
     idea
 }
 
@@ -71,54 +69,43 @@ sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("testresources")
 
 repositories {
-    jcenter()
-    maven {
-        url = uri("https://packages.confluent.io/maven/")
-    }
-    maven {
-        url = uri("https://jitpack.io")
-    }
-    maven {
-        url = uri("https://dl.bintray.com/mipt-npm/dev")
-    }
+    mavenCentral()
+    maven("https://jcenter.bintray.com/")
+    maven("https://packages.confluent.io/maven/")
+    maven("https://jitpack.io")
+    maven("https://dl.bintray.com/mipt-npm/dev")
     maven("https://dl.bintray.com/mipt-npm/dataforge")
     maven("https://dl.bintray.com/mipt-npm/kscience")
 }
 
 dependencies {
-    dependencies { kotlin("reflect") }
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
-
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
-
     implementation("io.ktor:ktor-jackson:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-
     implementation("org.flywaydb:flyway-core:$flywayVersion")
     implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
     implementation("no.nav:vault-jdbc:$vaultJdbcVersion")
-
     implementation("io.ktor:ktor-auth:$ktorVersion")
     implementation("no.nav.security:token-validation-ktor:$tokenValidationVersion") {
         exclude(group = "io.ktor", module = "ktor-auth")
     }
-
     implementation("io.ktor:ktor-metrics-micrometer:$ktorVersion")
     implementation("io.micrometer:micrometer-registry-prometheus:$micrometerPrometheusVersion")
-
     implementation("org.apache.kafka:kafka-clients:$kafkaClientsVersion")
     implementation("io.confluent:kafka-avro-serializer:$kafkaAvroSerializerVersion")
     implementation("net.javacrumbs.shedlock:shedlock-core:$shedlockVersion")
     implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc:$shedlockVersion")
+    implementation("kscience.plotlykt:plotlykt-server:$plotlyktServerVersion")
 
-    implementation("kscience.plotlykt:plotlykt-server:0.3.0")
-
+    testImplementation(kotlin("test"))
     testImplementation("no.nav.security:token-validation-test-support:$tokenValidationVersion") {
         exclude(group = "org.springframework.boot")
     }
@@ -130,12 +117,12 @@ dependencies {
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:$assertkVersion")
     testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvironmentVersion")
     testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
-    testImplementation("org.skyscreamer:jsonassert:1.5.0")
+    testImplementation("org.skyscreamer:jsonassert:$jsonassertVersion")
     testImplementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:$pitestVersion")
 }
 
 configurations.all {
     resolutionStrategy {
-        force("io.github.microutils:kotlin-logging:2.0.6")
+        force("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
     }
 }
