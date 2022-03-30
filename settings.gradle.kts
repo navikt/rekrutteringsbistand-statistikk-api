a4c1c8b79fb2d673b8f9e0842f5fa938702c2f57
Are Husby <are.husby@nav.no>
2022-03-30 10:21:10 +0200

    Oppdater gradlew + fix deprecation warning + la gradlew styre Kotlin-versjon

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ rootProject.name = "rekrutteringsbistand-statistikk-api"
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        jcenter()
-        maven (url="https://dl.bintray.com/gradle/gradle-plugins")
+        mavenCentral()
     }
 }
