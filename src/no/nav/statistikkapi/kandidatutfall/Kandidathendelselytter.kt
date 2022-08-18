83fdc87b887b03015635a44333ca387b01dd7b50
Mads Lee Giil <mads.lee.giil@nav.no>
2022-08-18 13:49:34 +0200

    Test at kandidathendelse lagres som utfall i databasen

--- src/no/nav/statistikkapi/kandidatutfall/Kandidathendelselytter.kt
+++ src/no/nav/statistikkapi/kandidatutfall/Kandidathendelselytter.kt
@@ -5,6 +5,7 @@ import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import no.nav.statistikkapi.log
+import java.time.ZonedDateTime
 
 class Kandidathendelselytter(rapidsConnection: RapidsConnection): River.PacketListener {
 
@@ -20,5 +21,25 @@ class Kandidathendelselytter(rapidsConnection: RapidsConnection): River.PacketLi
         log.info("Mottok en melding om at CV er delt med arbeidsgiver via rekrutteringsbistand men gjør ingenting med den!")
         // Vi kan ha gamle eventer som har utc tidssone. Kjør dette for sikkerhetsskyld på tidspunktene:
         // it.copy(tidspunktForHendelsen = it.tidspunktForHendelsen.withZoneSameInstant(ZoneId.of("Europe/Oslo")))
+    }
+
+    data class Kandidathendelse(
+        val type: Type,
+        val aktørId: String,
+        val organisasjonsnummer: String,
+        val kandidatlisteId: String,
+        val tidspunkt: ZonedDateTime,
+        val stillingsId: String,
+        val utførtAvNavIdent: String,
+        val utførtAvNavKontorKode: String,
+        val synligKandidat: Boolean,
+        val harHullICv: Boolean,
+        val alder: Int,
+        val tilretteleggingsbehov: List<String>,
+    )
+
+    enum class Type(private val eventNamePostfix: String) {
+        CV_DELT_UTENFOR_REKRUTTERINGSBISTAND("cv-delt-med-arbeidsgiver-utenfor-rekrutteringsbistand"),
+        CV_DELT_VIA_REKRUTTERINGSBISTAND("cv-delt-med-arbeidsgiver-via-rekrutteringsbistand");
     }
 }
--- test/no/nav/statistikkapi/LagreStatistikkTest.kt
+++ test/no/nav/statistikkapi/LagreStatistikkTest.kt
@@ -1,11 +1,17 @@
 package no.nav.statistikkapi
 
-import no.nav.helse.rapids_rivers.RapidsConnection
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
+import assertk.assertions.isNull
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.statistikkapi.db.TestDatabase
 import no.nav.statistikkapi.db.TestRepository
+import no.nav.statistikkapi.kandidatutfall.Kandidathendelselytter
+import no.nav.statistikkapi.kandidatutfall.SendtStatus
+import no.nav.statistikkapi.kandidatutfall.Utfall
 import org.junit.After
-import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
 
@@ -34,6 +40,54 @@ class LagreStatistikkTest {
         rapid.sendTestMessage(json)
     }
 
+    // given: gitt at det ligger en CV-delt-melding på topic
+    // when: når vi konsumerer
+    // then: skal det lagres et presentert-kandidatutfall i databasen
+    @Test
+    fun `en kandidathendelsemelding skal lagres som kandidatutfall i databasen`() {
+        val kandidathendelsemelding = kandidathendelseMap()
+        val kandidathendelsesmeldingJson = jacksonObjectMapper().writeValueAsString(kandidathendelsemelding)
 
+        rapid.sendTestMessage(kandidathendelsesmeldingJson)
 
+        val alleUtfall = testRepository.hentUtfall()
+        assertThat(alleUtfall.size).isEqualTo(1)
+
+        val actual = alleUtfall.first()
+        val kandidathendelse = kandidathendelsemelding["kandidathendelse"] as Map<*, *>
+        assertThat(actual.dbId).isNotNull()
+        assertThat(actual.utfall).isEqualTo(kandidathendelse["utfall"])
+        assertThat(actual.aktorId).isEqualTo(kandidathendelse["aktørId"])
+        assertThat(actual.alder).isEqualTo(kandidathendelse["alder"])
+        assertThat(actual.tidspunkt).isEqualTo(kandidathendelse["tidspunkt"])
+        assertThat(actual.hullICv).isEqualTo(kandidathendelse["harHullICv"])
+        assertThat(actual.kandidatlisteId).isEqualTo(kandidathendelse["kandidatlisteId"])
+        assertThat(actual.stillingsId).isEqualTo(kandidathendelse["stillingsId"])
+        assertThat(actual.navIdent).isEqualTo(kandidathendelse["utførtAvNavIdent"])
+        assertThat(actual.navKontor).isEqualTo(kandidathendelse["utførtAvNavKontorKode"])
+        assertThat(actual.tilretteleggingsbehov).isEqualTo(kandidathendelse["tilretteleggingsbehov"])
+        assertThat(actual.synligKandidat).isEqualTo(kandidathendelse["synligKandidat"])
+        assertThat(actual.utfall).isEqualTo(Utfall.PRESENTERT)
+        assertThat(actual.antallSendtForsøk).isEqualTo(0)
+        assertThat(actual.sendtStatus).isEqualTo(SendtStatus.IKKE_SENDT)
+        assertThat(actual.sisteSendtForsøk).isNull()
+    }
+
+    fun kandidathendelseMap() = mapOf(
+        "@event_name" to "kandidat.cv-delt-med-arbeidsgiver-via-rekrutteringsbistand",
+        "kandidathendelse" to mapOf(
+            "type" to "CV_DELT_VIA_REKRUTTERINGSBISTAND",
+            "aktørId" to "dummyAktørid",
+            "organisasjonsnummer" to "123456789",
+            "kandidatlisteId" to "24e81692-37ef-4fda-9b55-e17588f65061",
+            "tidspunkt" to "2022-08-18T10:33:02.5+02:00",
+            "stillingsId" to "b3c925af-ebf4-50d1-aeee-efc9259107a4",
+            "utførtAvNavIdent" to "Z994632",
+            "utførtAvNavKontorKode" to "0313",
+            "synligKandidat" to true,
+            "harHullICv" to true,
+            "alder" to 62,
+            "tilretteleggingsbehov" to listOf("arbeidstid")
+        )
+    )
 }
