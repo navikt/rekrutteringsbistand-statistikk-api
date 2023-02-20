ff5ee0ca4376fb64c205711b3f7234a75aae3967
Mads Lee Giil <mads.lee.giil@nav.no>
2023-02-20 08:43:20 +0100

    Legg til nye tester for slettetStillingOgKandidatliste-meldinger

--- test/no/nav/statistikkapi/hendelser/SlettetStillingOgKandidatlisteLytterTest.kt
+++ test/no/nav/statistikkapi/hendelser/SlettetStillingOgKandidatlisteLytterTest.kt
@@ -6,11 +6,14 @@ import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.statistikkapi.*
 import no.nav.statistikkapi.db.TestDatabase
 import no.nav.statistikkapi.db.TestRepository
+import no.nav.statistikkapi.kandidatutfall.Kandidatutfall
 import no.nav.statistikkapi.kandidatutfall.KandidatutfallRepository
 import no.nav.statistikkapi.kandidatutfall.Utfall
+import no.nav.statistikkapi.kandidatutfall.Utfall.PRESENTERT
 import org.junit.After
 import org.junit.BeforeClass
 import org.junit.Test
+import java.util.*
 
 class SlettetStillingOgKandidatlisteLytterTest {
 
@@ -36,7 +39,7 @@ class SlettetStillingOgKandidatlisteLytterTest {
 
     @Test
     fun `Kan annullere kandidatutfall med SlettetStillingOgKandidatliste-melding`() {
-        val utfallPresentert = etKandidatutfall.copy(utfall = Utfall.PRESENTERT, aktørId = aktørId1)
+        val utfallPresentert = etKandidatutfall.copy(utfall = PRESENTERT, aktørId = aktørId1)
         val utfallFåttJobben = utfallPresentert.copy(utfall = Utfall.FATT_JOBBEN, aktørId = aktørId2)
         repository.lagreUtfall(utfallPresentert)
         repository.lagreUtfall(utfallFåttJobben)
@@ -68,18 +71,44 @@ class SlettetStillingOgKandidatlisteLytterTest {
 
     @Test
     fun `Skal ikke lagre nytt utfall hvis siste utfall for en kandidat er IKKE_PRESENTERT`() {
+        val utfallPresentert = etKandidatutfall.copy(utfall = PRESENTERT, aktørId = aktørId1)
+        val utfallIkkePresentert = utfallPresentert.copy(utfall = Utfall.IKKE_PRESENTERT, aktørId = aktørId2)
+        repository.lagreUtfall(utfallPresentert)
+        repository.lagreUtfall(utfallIkkePresentert)
+        assertThat(testRepository.hentUtfall()).hasSize(2)
 
+        rapid.sendTestMessage(slettetStillingOgKandidatlisteMelding(utfallIkkePresentert.kandidatlisteId))
+
+        val utfallFraDatabase = testRepository.hentUtfall()
+        assertThat(utfallFraDatabase).hasSize(3)
+        val nyttUtfall = utfallFraDatabase.sortedByDescending { it.tidspunkt }.first()
+        assertThat(nyttUtfall.aktorId).isEqualTo(utfallPresentert.aktørId)
+        assertThat(nyttUtfall.utfall).isEqualTo(Utfall.IKKE_PRESENTERT)
     }
 
     @Test
     fun `Skal ikke lagre nye utfall for andre kandidatlister`() {
+        val utfallPresentert = etKandidatutfall.copy(utfall = PRESENTERT, aktørId = aktørId1)
+        val utfallPresentertAnnenKandidatliste = etKandidatutfall.copy(kandidatlisteId = UUID.randomUUID().toString(), utfall = PRESENTERT, aktørId = aktørId1)
+        repository.lagreUtfall(utfallPresentert)
+        repository.lagreUtfall(utfallPresentertAnnenKandidatliste)
+        assertThat(testRepository.hentUtfall()).hasSize(2)
+
+        rapid.sendTestMessage(slettetStillingOgKandidatlisteMelding(utfallPresentert.kandidatlisteId))
+
+        val utfallFraDatabase = testRepository.hentUtfall()
+        assertThat(utfallFraDatabase).hasSize(3)
+        val nyttUtfall = utfallFraDatabase.sortedByDescending { it.tidspunkt }.first()
+        assertThat(nyttUtfall.kandidatlisteId.toString()).isEqualTo(utfallPresentert.kandidatlisteId)
+        assertThat(nyttUtfall.aktorId).isEqualTo(utfallPresentert.aktørId)
+        assertThat(nyttUtfall.utfall).isEqualTo(Utfall.IKKE_PRESENTERT)
     }
 
     private fun slettetStillingOgKandidatlisteMelding(kandidatlisteId: String) = """
         {
           "organisasjonsnummer": "312113341",
           "kandidatlisteId": "$kandidatlisteId",
-          "tidspunkt": "2023-02-09T09:45:53.649+01:00",
+          "tidspunkt": "${nowOslo()}",
           "stillingsId": "b5919e46-9882-4b3c-8089-53ad02f26023",
           "utførtAvNavIdent": "Z994633",
           "@event_name": "kandidat_v2.SlettetStillingOgKandidatliste",
@@ -87,5 +116,4 @@ class SlettetStillingOgKandidatlisteLytterTest {
           "@opprettet": "2023-02-09T09:46:01.027221527"
         }
     """.trimIndent()
-
 }
