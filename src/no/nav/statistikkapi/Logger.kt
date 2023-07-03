92c0c66b7fff8302fe43c4741661a0eed35f7669
Are Husby <are.husby@nav.no>
2023-07-03 21:07:18 +0200

    Eksperimenterer med en måte å logge til secureLog på som både bevarer info om hvilken klasse om logger og reduserer sannsynligheten for at info som er ment til secureLog havner i ordinær appLog

--- src/no/nav/statistikkapi/Logger.kt
+++ src/no/nav/statistikkapi/Logger.kt
@@ -2,8 +2,38 @@ package no.nav.statistikkapi
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.slf4j.Marker
+import org.slf4j.MarkerFactory
 
 val Any.log: Logger
     get() = LoggerFactory.getLogger(this::class.java)
 
 val secureLog = LoggerFactory.getLogger("secureLog")
+
+
+interface LoggerWithMarker {
+    fun info(msg: String)
+
+    fun info(msg: String, t: Throwable)
+}
+
+interface SecureLogLogger : LoggerWithMarker
+interface AppLogLogger : LoggerWithMarker
+
+
+private class LoggerWithMarkerImpl(private val m: Marker, private val l: Logger) : AppLogLogger, SecureLogLogger {
+    override fun info(msg: String) {
+        l.info(m, msg)
+    }
+
+    override fun info(msg: String, t: Throwable) {
+        l.info(m, msg, t)
+    }
+
+}
+
+val Any.aresAppLog: AppLogLogger
+    get() = LoggerWithMarkerImpl(MarkerFactory.getMarker("appLog"), LoggerFactory.getLogger(this::class.java))
+
+val Any.aresSecureLog: SecureLogLogger
+    get() = LoggerWithMarkerImpl(MarkerFactory.getMarker("secureLog"), LoggerFactory.getLogger(this::class.java))
